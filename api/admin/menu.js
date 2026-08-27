d8a210108f1b5a431dfdd41089d4a66d9aea38ec
AimeYummy <4ime.yummy@gmail.com>
2026-08-28 03:20:58 +0800

    Delete api/admin directory

--- api/admin/menu.js
+++
@@ -1,8 +0,0 @@
-import { listMenuItems } from '../../lib/server/_menu-store.js'
-import { requireAdmin } from '../../lib/server/_shared.js'
-export default async function handler(req,res){
-  if(req.method!=='GET')return res.status(405).json({message:'Method not allowed'})
-  if(!await requireAdmin(req,res))return
-  try{ return res.status(200).json({items:await listMenuItems(false)}) }
-  catch(e){ return res.status(500).json({message:e.message}) }
-}
